1d76e5fbb3b03dc5eadfd39b704848babb9156f1
Abdul Rasheed Esa <rasheed3086@gmail.com>
2023-11-17 03:59:08 +0530

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add your JavaScript code here
+
+// Example: Function to add a note
+function addNote() {
+    // Logic to add a note
+}
+
+// Example: Function to edit a note
+function editNote() {
+    // Logic to edit a note
+}
+
+// Example: Function to delete a note
+function deleteNote() {
+    // Logic to delete a note
+}
